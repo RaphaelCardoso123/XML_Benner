6241182bf01fcd2a8694caac13fbd9ac8e3180bc
RaphaelCardoso123 <91799151+RaphaelCardoso123@users.noreply.github.com>
2022-11-14 11:41:59 -0300

    ...

---
+++ XML_Benner/FMensagem/FCorpo/ABI/Prestadores/Prestador/Atendimento/Procedimentos/Procedimento.cs
@@ -0,0 +1,14 @@
+﻿
+namespace XML_Benner.FMensagem.FCorpo.ABI.Prestadores.Prestador.Atendimento.Procedimentos
+{
+    public class Procedimento
+    {
+
+        public string TipoProcedimento { get; set; }
+        public int CodigoProcedimento { get; set; }
+        public string DescricaoProcedimento { get; set; }
+        public int QuantidadeProcedimento { get; set; }
+        public double ValorProcedimento { get; set; }
+
+    }
+}
